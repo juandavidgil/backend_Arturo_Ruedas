4dfb03c57e5ab5fcde5bbf025d62dc4c49af91b9
juandavidgil <juandavidgil0911@gmail.com>
2025-09-29 16:40:02 -0600

    correccion import

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import dotenv from "dotenv";
 import { Expo } from "expo-server-sdk";
 import { createClient } from '@supabase/supabase-js';
 import { Resend } from "resend";
+import admin from 'firebase-admin';
 
 
 dotenv.config();
@@ -22,7 +23,6 @@ if (!admin.apps.length) {
 }
 
 
-import admin from 'firebase-admin';
 
 export const supabase = createClient(
   process.env.SUPABASE_URL!,
